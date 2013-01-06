ae7ccbc7214fd2f318096aaa6add8ca8d2ffc9b4
sabotrax <sabotrax@gmail.com>
2013-01-06 12:50:40 +0100

    - added simple downtime handling.

---
+++ downtime.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby -w
+
+# This file is part of indypicdump.
+
+# indypicdump is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# indypicdump is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with indypicdump.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2012 Marcus Schommer <sabotrax@gmail.com>
+
+$:.unshift("/home/schommer/dev/indypicdump")
+
+require 'sinatra'
+require 'slim'
+require 'ipdconfig'
+
+set :environment, IPDConfig::ENVIRONMENT
+
+##############################
+get '/' do
+  slim :downtime, :pretty => IPDConfig::RENDER_PRETTY, :layout => false
+end
